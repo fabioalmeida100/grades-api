1799293ad7fbe6816ef18ef51c98ef8808613199
Fábio Almeida <fabio_almeida100@hotmail.com>
2020-08-08 14:14:20 -0300

    Add endpoint /update

--- routes/grades.js
+++ routes/grades.js
@@ -9,7 +9,7 @@ router.post('/create', async (req, res) => {
       const grade = req.body;
       const dbJson = JSON.parse(await readFile(global.dataBaseFile));
       
-      let newGrade = {
+      let gradeNew = {
         id: dbJson.nextId++,
         student: grade.student,
         subject: grade.subject,
@@ -18,13 +18,42 @@ router.post('/create', async (req, res) => {
         timestamp: new Date()
       };
       
-      dbJson.grades.push(newGrade);
+      dbJson.grades.push(gradeNew);
       await writeFile(global.dataBaseFile, JSON.stringify(dbJson, null, 2));
 
-      res.send(newGrade);
+      res.send(gradeNew);
     } catch (error) {
-      res.status(400).send({"error": error});
+      res.status(400).send({"error": error.message});
     }
-})
+});
+
+router.patch("/update", async (req, res) => {
+    try {
+      const grade = req.body;
+      const dbJson = JSON.parse(await readFile(global.dataBaseFile));
+              
+      const index = dbJson.grades.findIndex(a => a.id === grade.id);
+      if (index === -1) {
+          throw new Error("Registro não encontrado.");
+      }
+
+      let gradeUpdated = {
+        id: dbJson.grades[index].id,
+        student: grade.student,
+        subject: grade.subject,
+        type: grade.type,
+        value: grade.value,
+        timestamp: dbJson.grades[index].timestamp
+      };
+
+      dbJson.grades[index] = gradeUpdated;
+
+      await writeFile(global.dataBaseFile, JSON.stringify(dbJson, null, 2));
+
+      res.send(dbJson.grades[index]);
+    } catch (error) {
+      res.status(400).send({"error": error.message});
+    }
+});
 
 export default router;
